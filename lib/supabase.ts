4e880eb7a449b31942644b13f0437dc749cd0fe6
Amro Hendawi <amro.hendawi@gmail.com>
2025-04-08 15:10:43 +0200

    removed vercel.json

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -10,17 +10,5 @@ if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  {
-    auth: {
-      persistSession: false,
-      autoRefreshToken: false,
-    },
-    global: {
-      fetch: fetch.bind(globalThis),
-    },
-    db: {
-      schema: 'public',
-    },
-  }
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
