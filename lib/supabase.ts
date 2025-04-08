f14f5922f4a6c31c75102b29f88e31c04b71e64c
Amro Hendawi <amro.hendawi@gmail.com>
2025-04-08 14:47:51 +0200

    supabase timeout

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -10,5 +10,17 @@ if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+    },
+    global: {
+      fetch: fetch.bind(globalThis),
+    },
+    db: {
+      schema: 'public',
+    },
+  }
 );
